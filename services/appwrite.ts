6cd13c0b87f9502f7a025d2018f32b3cece58dd7
henokmulat <henokmulat27@gmail.com>
2025-10-31 12:46:37 +0300

    Implement Appwrite-based search tracking with document create/update logic for movie searches

---
+++ services/appwrite.ts
@@ -0,0 +1,45 @@
+import { Client, Databases, Query } from "react-native-appwrite";
+
+// track the searches made by the user
+
+const DATABASE_ID = process.env.EXPO_PUBLIC_APPWRITE_DATABASE_ID!;
+const COLLECTION_ID = process.env.EXPO_PUBLIC_APPWRITE_COLLECTION_ID!;
+//"https://cloud.appwrite.io/v1"
+const client = new Client()
+  .setEndpoint(process.env.EXPO_PUBLIC_APPWRITE_ENDPOINT!)
+  .setProject(process.env.EXPO_PUBLIC_APPWRITE_PROJECT_ID!);
+
+const databases = new Databases(client);
+export const updateSearchCount = async (query: string, movie: Movie) => {
+  try {
+    const result = await databases.listDocuments(DATABASE_ID, COLLECTION_ID, [
+      Query.equal("searchTerm", query),
+    ]);
+    // check if the record of that search has already been stored
+    if (result.documents.length > 0) {
+      console.log("✅ Updating existing document:", result.documents[0].$id);
+      const existingMovie = result.documents[0];
+      await databases.updateDocument(
+        DATABASE_ID,
+        COLLECTION_ID,
+        existingMovie.$id,
+        {
+          count: existingMovie.count + 1,
+        }
+      );
+    } else {
+      console.log("🆕 Creating new document for query:", query);
+      const uniqueId = Math.random().toString(36).substring(2, 12);
+      await databases.createDocument(DATABASE_ID, COLLECTION_ID, uniqueId, {
+        searchTerm: query,
+        movie_id: movie.id,
+        count: 1,
+        title: movie.title,
+        poster_url: `https://image.tmdb.org/t/p/w500${movie.poster_path}`,
+      });
+    }
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+};
