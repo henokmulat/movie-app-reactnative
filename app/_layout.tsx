ba092ae306bdedf2bd1e7f186a6dfbc7ae5d193a
henokmulat <henokmulat27@gmail.com>
2025-12-04 23:52:04 +0300

    Hide header on trailer screen by setting headerShown to false

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -19,6 +19,7 @@ export default function RootLayout() {
             headerTintColor: "#fff",
           }}
         />
+        <Stack.Screen name="trailer" options={{ headerShown: false }} />
       </Stack>
     </>
   );
